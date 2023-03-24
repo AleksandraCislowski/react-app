13754f4047431123666b70b0527524346f5b8175
aleksandra <aleksandra.swigon91@gmail.com>
2023-03-24 18:05:22 +0100

    adding props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore React' />
     </div>
   );
 };
