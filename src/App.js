e33da0b07d6f143f40b8ec1f84ce57660eddbc5c
aleksandra <aleksandra.swigon91@gmail.com>
2023-03-26 10:24:44 +0200

    extra styling, new button

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ const App = () => {
   return (
     <div>
       <h1>My Todos</h1>
+      <button className='btn--main'>Add new task</button>
       <Todo text='Learn React' />
+      <br></br>
       <Todo text='Master React' />
+      <br></br>
       <Todo text='Explore React' />
+      <br></br>
     </div>
   );
 };
